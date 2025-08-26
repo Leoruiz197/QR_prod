8cec80cf84fe3c3f1eeb0e755e85165a43a61483
Augusto579 <gcw118.acs@gmail.com>
2025-08-26 10:08:35 -0300

    novas mudanças

--- gerador_qr.py
+++ gerador_qr.py
@@ -1,18 +1,22 @@
 import qrcode
 
-data = input("Digite o nome do produto: ")
+def gerar_qr(nome_produto):
+    data = input("Digite o nome do produto: ")
 
-qr = qrcode.QRCode(
-    version =1, 
-    error_correction = qrcode.constants.ERROR_CORRECT_H,
-    box_size = 10, 
-    border = 4
-)
+    qr = qrcode.QRCode(
+        version =1, 
+        error_correction = qrcode.constants.ERROR_CORRECT_H,
+        box_size = 10, 
+        border = 4
+    )
 
-qr.add_data(data)
-qr.make(fit = True)
+    qr.add_data(nome_produto)
+    qr.make(fit = True)
 
-img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white")
 
-img.save(f"./qrcodes/{data}.png")
-print("QRcode gerado com sucesso")
+    img.save(f"./qrcodes/{nome_produto}.png")
+    print("QRcode gerado com sucesso")
+
+if __name__ == "__main__":
+    gerar_qr()
